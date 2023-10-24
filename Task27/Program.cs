9a143c5eb68b00d29be109f1fa69f75d19521503
Pavel <pavel@b2profit.ru>
2023-10-24 20:18:36 +0300

    Решил задачу 27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int number =Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int CountNumbers(int newNumber)
+{
+   int count = 0;
+    while(newNumber > 0)
+    {
+        newNumber/=10;
+        count++;
+    }
+    return count-1;
+}
+
+int SumNumbers(int newNum)
+{
+    int sum = 0;
+    for(int i=0; i<=CountNumbers(newNum); i++)
+    {
+        
+        sum += newNum / (int)Math.Pow(10, CountNumbers(newNum));
+        newNum %= (int)Math.Pow(10, CountNumbers(newNum));
+    }
+        return sum + newNum;
+}
+
+Console.WriteLine(SumNumbers(number));
