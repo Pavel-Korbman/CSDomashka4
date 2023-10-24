524f9581d85497d51a0a23ec3e95596ec6960d10
Pavel <pavel@b2profit.ru>
2023-10-24 20:51:47 +0300

    Решил задачу 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void WriteArray(int [] newArray)
+{
+// int stop = newArray.Length;
+  for(int i=0; i<newArray.Length; i++)
+  {
+  Console.Write(newArray[i]+ ", ");
+  }
+}
+int [] Array = {1, 2, 5, 7, 19};
+WriteArray(Array);
+Console.WriteLine();
+int [] Array1 = {6, 1, 33};
+WriteArray(Array1);
