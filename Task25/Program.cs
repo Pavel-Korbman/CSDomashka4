60448ac554f5731d224adb7bc38dafac472db41c
Pavel <pavel@b2profit.ru>
2023-10-24 17:37:06 +0300

    Решил 25 задачу

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int pow = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+if(pow < 1) Console.WriteLine("Cтепень должна быть натуральным числом");
+else
+{
+for(int i = 1; i <= pow; i ++)
+{
+result = result * number;
+}
+Console.WriteLine(result);
+}
